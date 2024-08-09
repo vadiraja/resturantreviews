f235730be1c4ba15c8e8943b226d602a2f777287
vrao <vadiraja.a@gmail.com>
2024-08-09 13:34:42 -0500

    fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,52 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
-import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
-import { routes } from './app.routes';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
+import { provideAuth, getAuth, connectAuthEmulator } from "@angular/fire/auth";
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from "@angular/fire/firestore";
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from "@angular/fire/functions";
+import { provideMessaging, getMessaging } from "@angular/fire/messaging";
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from "@angular/fire/storage";
+import { routes } from "./app.routes";
+import { provideRouter } from "@angular/router";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "tunturu-2b497",
+        appId: "1:658070131735:web:c18a47ffb04e88cfbe3852",
+        storageBucket: "tunturu-2b497.appspot.com",
+        // locationId: "us-central",
+        apiKey: "AIzaSyCFEfwZB9qGxlL8IhLZF_I2VU6UxQmg--U",
+        authDomain: "tunturu-2b497.firebaseapp.com",
+        messagingSenderId: "658070131735",
+        measurementId: "G-796VSYSGH7",
+      }),
     ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"tunturu-2b497","appId":"1:658070131735:web:c18a47ffb04e88cfbe3852","storageBucket":"tunturu-2b497.appspot.com","locationId":"us-central","apiKey":"AIzaSyCFEfwZB9qGxlL8IhLZF_I2VU6UxQmg--U","authDomain":"tunturu-2b497.firebaseapp.com","messagingSenderId":"658070131735","measurementId":"G-796VSYSGH7"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
+    // importProvidersFrom(
+    //   provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //   provideFirestore(() => getFirestore()),
+    //   provideAuth(() => getAuth()),
+    //   provideFunctions(() => getFunctions()),
+    //   provideStorage(() => getStorage()),
+    //   provideMessaging(() => getMessaging()),
+    // ),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyCFEfwZB9qGxlL8IhLZF_I2VU6UxQmg--U",
     authDomain: "tunturu-2b497.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    databaseURL: "https://tunturu-2b497.firebaseio.com",
     projectId: "tunturu-2b497",
     storageBucket: '"tunturu-2b497.appspot.com',
-    messagingSenderId: "1:658070131735:web:c18a47ffb04e88cfbe3852",
-    appId: "APP_ID",
+    messagingSenderId: "658070131735",
+    appId: "1:658070131735:web:6b61d67073960108be3852",
     measurementId: "G-796VSYSGH7",
   },
 };
